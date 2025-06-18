b3aa26fc5caa2f30da8f679a6b58a9b2dbd0e5de
ZeoHub <romanozchristian0@gmail.com>
2025-06-18 22:02:07 +0800

    Update Loader.lua

--- GrowAGarden/Loader.lua
+++ GrowAGarden/Loader.lua
@@ -242,10 +242,10 @@ showZeoHubLoadingScreen(function()
     local closeBtn = Instance.new("TextButton", topBar)
     closeBtn.Name = "CloseButton"
     closeBtn.Size = UDim2.new(0, 28, 0, 28)
-    closeBtn.Position = UDim2.new(1, -68, 0, 4)
+    closeBtn.Position = UDim2.new(1, -68, 3, 4)
     closeBtn.AnchorPoint = Vector2.new(0,0)
     closeBtn.BackgroundColor3 = Color3.fromRGB(200, 65, 65)
-    closeBtn.Text = "✕"
+    closeBtn.Text = "X"
     closeBtn.Font = Enum.Font.GothamBold
     closeBtn.TextSize = 18
     closeBtn.TextColor3 = Color3.new(1,1,1)
